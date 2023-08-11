e394f201c309c238d81b818b379f82c04e160f8f
contatopolianamarques@gmail.com <contatopolianamarques@gmail.com>
2023-08-11 20:01:27 -0300

    inserindo tag span com descricao de redirecionamento

--- gamp-recipes/app/profile/page.tsx
+++ gamp-recipes/app/profile/page.tsx
@@ -5,11 +5,19 @@ import Link from "next/link"
 export default function ProfilePage() {
   return (
     <>
-    <h2>Name:</h2>
-  
-    <Image src="/icons/bob.jpg" alt="Bob esponja calca quadrada segurando uma espatacula com um hambuguer" width={50} height={50}/>
-    <Link href="/favoriteRecipes"><Image src="/icons/coracao.png" alt="icone de coracao" width={20} height={20}/></Link>
-    <Link href="/inProgress"><Image src="/icons/cozinhando.png" alt="icone de coracao" width={20} height={20}/></Link>
+      <h2>Name:</h2>
+
+      <Image src="/icons/bob.jpg" alt="Bob esponja calca quadrada segurando uma espatacula com um hambuguer" width={50} height={50} />
+      <Link href="/favoriteRecipes">
+        <span> Favorite Recipes
+        <Image src="/icons/coracao.png" alt="icone de coracao" width={20} height={20} />
+        </span>
+      </Link>
+      <Link href="/inProgress">
+        <span>In Progress
+        <Image src="/icons/cozinhando.png" alt="icone de coracao" width={20} height={20} />
+        </span>
+      </Link>
     </>
   )
 }
