678fedb5e9c2c111cafce7653d6a43eb1e033ee1
contatopolianamarques@gmail.com <contatopolianamarques@gmail.com>
2023-08-14 18:21:47 -0300

    Formatando o codigo

--- gamp-recipes/app/profile/page.tsx
+++ gamp-recipes/app/profile/page.tsx
@@ -5,13 +5,14 @@ import { useContext } from "react"
 
 export default function ProfilePage() {
   const { user } = useContext(UserContext);
+
   return (
     <>
       <Image src="/icons/bob.jpg" alt="Bob esponja calca quadrada segurando uma espatacula com um hambuguer" width={50} height={50} />
 
       <h2>Name:{user?.username}</h2>
       <h2>Email: {user?.email}</h2>
-      
+
       <Link href="/favoriteRecipes">
         <span> Favorite Recipes
           <Image src="/icons/coracao.png" alt="icone de coracao" width={20} height={20} />
