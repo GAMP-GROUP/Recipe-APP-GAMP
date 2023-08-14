38628af588d58938ae70f86371af7e2b5685352b
contatopolianamarques@gmail.com <contatopolianamarques@gmail.com>
2023-08-14 15:02:02 -0300

    Buscando username e email do login para o profile

--- gamp-recipes/app/profile/page.tsx
+++ gamp-recipes/app/profile/page.tsx
@@ -1,21 +1,25 @@
 import Image from "next/image"
 import Link from "next/link"
-
+import { UserContext } from "../context"
+import { useContext } from "react"
 
 export default function ProfilePage() {
+  const { user } = useContext(UserContext);
   return (
     <>
-      <h2>Name:</h2>
-
       <Image src="/icons/bob.jpg" alt="Bob esponja calca quadrada segurando uma espatacula com um hambuguer" width={50} height={50} />
+
+      <h2>Name:{user?.username}</h2>
+      <h2>Email: {user?.email}</h2>
+      
       <Link href="/favoriteRecipes">
         <span> Favorite Recipes
-        <Image src="/icons/coracao.png" alt="icone de coracao" width={20} height={20} />
+          <Image src="/icons/coracao.png" alt="icone de coracao" width={20} height={20} />
         </span>
       </Link>
       <Link href="/inProgress">
         <span>In Progress
-        <Image src="/icons/cozinhando.png" alt="icone de coracao" width={20} height={20} />
+          <Image src="/icons/cozinhando.png" alt="icone de coracao" width={20} height={20} />
         </span>
       </Link>
     </>
