c00b905d31bb234b0ac9b507cda2d4a2651c8886
gabesouto <soutogabriel04@gmail.com>
2023-08-15 15:27:57 -0300

    feat: lib file, endpoints, fetch types

--- gamp-recipes/app/page.tsx
+++ gamp-recipes/app/page.tsx
@@ -4,11 +4,18 @@ import { UserContext } from "@/contextAPI/context";
 import { useContext } from "react";
 import LoginForm from "./components/loginForm";
 
+
 export default function Home() {
   const {logging} = useContext(UserContext)
+
   return (
       <div>
         <h1>RECIPES</h1>
+        <div>
+          {
+
+          }
+        </div>
         {
         logging &&
         <LoginForm />
