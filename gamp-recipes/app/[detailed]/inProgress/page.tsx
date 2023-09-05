9060bdb374527fd6c88e2e2423a2e71d916c9c3c
Gabriel <soutogabriel04@gmail.com>
2023-09-05 13:47:52 -0300

    feat:[InProgress page]

--- gamp-recipes/app/[detailed]/inProgress/page.tsx
+++ gamp-recipes/app/[detailed]/inProgress/page.tsx
@@ -1,52 +1,34 @@
-import { formatResponse } from '@/app/lib/formatResponse';
-import { getMealById, getDrinkById } from '@/app/lib/externalAPI';
-import Image from 'next/image';
-import { detailedParams } from "@/types"
+import { formatResponse } from "@/app/lib/formatResponse";
+import { getMealById, getDrinkById } from "@/app/lib/externalAPI";
+import Image from "next/image";
+import { detailedParams } from "@/types";
+import IngredientList from "@/app/components/IngredientLIst";
+export default async function InProgress({
+  params: { detailed },
+}: detailedParams) {
+  const [api, id] = detailed.split("-");
 
-export default async function InProgress({ params: { detailed } }: detailedParams) {
+  if (api !== "meal" && api !== "drink") return <h1>Invalid route {api}</h1>;
 
-  const [api, id] = detailed.split('-');
-
-  if (api !== 'meal' && api !== 'drink') return <h1>Invalid route {api}</h1>
-
-  const fetchRecipe = api === 'meal' ? await getMealById(id) : await getDrinkById(id);
+  const fetchRecipe =
+    api === "meal" ? await getMealById(id) : await getDrinkById(id);
   const recipe = formatResponse(fetchRecipe, api);
 
   return (
     <div>
       <section>
-
         <picture>
-        <img src={recipe.thumb} alt='recipe image'></img>
+          <img className="w-40" src={recipe.thumb} alt="recipe image"></img>
         </picture>
         <h2>{recipe.title}</h2>
         <br />
-        
-        <p>
-          {recipe.instructions}
-        </p>
 
+        <p>{recipe.instructions}</p>
       </section>
 
-    <section>
-    <ul className="space-y-4 text-left text-gray-500 dark:text-gray-400">
-      {
-        recipe.ingredients.map((ingredient, index) => {
-          return (
-      
-    <li className="flex items-center space-x-3" key={index}>
-        <svg className="flex-shrink-0 w-3.5 h-3.5 text-green-500 dark:text-green-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 16 12">
-            <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 5.917 5.724 10.5 15 1.5"/>
-        </svg>
-        <span>{ingredient}</span>
-    </li>
-          )
-      })
-}
-</ul>
-
-    </section>
-
+      <section>
+        <IngredientList ingredients={recipe.ingredients} />
+      </section>
     </div>
-  )
+  );
 }
--- gamp-recipes/app/lib/formatResponse.ts
+++ gamp-recipes/app/lib/formatResponse.ts
@@ -1,22 +1,25 @@
 import { MealRes, DrinkRes } from "@/types";
 
-type FormatRecipe = {
-  type: string,
-  category: string,
-  instructions: string,
-  tags: string,
-  id: string,
-  title: string,
-  thumb: string,
-  ingredients: string[],
-  measurements: string[],
-  isAlcoholic: string,
-  videoURL: string,
-}
+export type FormatRecipe = {
+  type: string;
+  category: string;
+  instructions: string;
+  tags: string;
+  id: string;
+  title: string;
+  thumb: string;
+  ingredients: string[];
+  measurements: string[];
+  isAlcoholic: string;
+  videoURL: string;
+};
 
-export function formatResponse(recipe: MealRes | DrinkRes, api: string): FormatRecipe{
+export function formatResponse(
+  recipe: MealRes | DrinkRes,
+  api: string
+): FormatRecipe {
   const detailed = recipe;
-  const inDrink = api === 'drink';
+  const inDrink = api === "drink";
   const render = new Map<string, string | string[]>([
     ["type", api],
     ["category", detailed.strCategory],
@@ -27,21 +30,21 @@ export function formatResponse(recipe: MealRes | DrinkRes, api: string): FormatR
     ["thumb", inDrink ? detailed.strDrinkThumb : detailed.strMealThumb],
   ]);
   inDrink
-  ? render.set("isAlcoholic", detailed.strAlcoholic) 
-  : render.set("videoURL", detailed.strYoutube.split('=')[1])
-  const range = api === 'drink' ? 15 : 20
-  const measurements = []
-  const ingredients = []
+    ? render.set("isAlcoholic", detailed.strAlcoholic)
+    : render.set("videoURL", detailed.strYoutube.split("=")[1]);
+  const range = api === "drink" ? 15 : 20;
+  const measurements = [];
+  const ingredients = [];
   for (let i = 1; i <= range; i += 1) {
     const strIng = "strIngredient" + i;
-    const strMes = 'strMeasure' + i;
+    const strMes = "strMeasure" + i;
     const ingRef = strIng in detailed ? detailed[strIng] : null;
-    if (ingRef !== null && ingRef !== undefined && ingRef !== '') {
+    if (ingRef !== null && ingRef !== undefined && ingRef !== "") {
       ingredients.push(detailed[strIng]);
       measurements.push(detailed[strMes]);
     }
   }
   render.set("ingredients", ingredients);
-  render.set("measurements", measurements)
+  render.set("measurements", measurements);
   return Object.fromEntries(render) as FormatRecipe;
 }
