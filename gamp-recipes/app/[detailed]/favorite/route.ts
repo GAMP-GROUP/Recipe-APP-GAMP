bf61c9d28dc3ff1a457083e938892ce606f7e47b
Andrey Tetsuo <tetsuo_miyasato@hotmail.com>
2023-10-05 21:55:17 -0300

    feat(favorite POST): adicionada verificações iniciais da request para favoritar uma receita, TODO: passar para middleware com intuito de reutilização em outras rotas

--- gamp-recipes/app/[detailed]/favorite/route.ts
+++ gamp-recipes/app/[detailed]/favorite/route.ts
@@ -1,31 +1,68 @@
+import { decode } from '@/app/lib/jwtUtils';
 import prisma from '@/prisma/client';
 import { NextRequest, NextResponse } from 'next/server';
  
 export async function POST(request: NextRequest) {
-	//Recebe id do POST request disparado pelo FavoriteButton
-	const { id } = await request.json(); 
-	//Explorar uma maneira de passar a chave primária do usuário no POST
-	const user_id = 1;
-
-	if(!id) return NextResponse.json({ message: 'Id is missing in Request', error: 'INVALID_INPUT'});
-	
-	const favoriteRecipe = await prisma.favorite_Recipes.findFirst({
-		where: { user_id,	recipe_id: Number(id), }});
-
-	if (favoriteRecipe === null) {
-		const created = await prisma.favorite_Recipes.create({
-			data: { user_id,	recipe_id: Number(id), fav: true }
+	try {
+		const cookie = request.cookies.get('next-auth.session-token');
+		
+		if (!cookie) return NextResponse.json(
+			{	
+				message: 'Unauthorized, access denied',
+				error: 'UNAUTHORIZED'
+			},
+			{ status: 401 },
+		);
+		
+		const payload = {
+			token: cookie.value,
+			secret: process.env.NEXTAUTH_SECRET as string,
+		};
+		
+		const auth = await decode(payload);
+		
+		const { id } = await request.json();
+		
+		const user = await prisma.user.findUnique({
+			where: { email: auth.email }
 		});
 
-		return NextResponse.json({message: created});
-	}
+		if(user === null) return NextResponse.json(
+			{
+				message: 'User not Found!', 
+				error: 'NOT_FOUND'
+			},
+			{ status: 404 }
+		);
 
-	if (favoriteRecipe !== null) {
-		const updated = await prisma.favorite_Recipes.update({
-			where: { id: favoriteRecipe?.id },
-			data: { fav: !favoriteRecipe.fav }
-		});
-
-		return NextResponse.json({message: updated });
+		if(!id) return NextResponse.json(
+			{
+				message: 'Id is missing in Request',
+				error: 'INVALID_INPUT'
+			},
+			{ status: 400}
+		);
+		
+		const favoriteRecipe = await prisma.favorite_Recipes.findFirst({
+			where: { user_id: user.id,	recipe_id: Number(id), }});
+			
+		if (favoriteRecipe === null) {
+			const created = await prisma.favorite_Recipes.create({
+				data: { user_id: user.id,	recipe_id: Number(id), fav: true }
+			});
+				
+			return NextResponse.json({message: created});
+		}
+			
+		if (favoriteRecipe !== null) {
+			const updated = await prisma.favorite_Recipes.update({
+				where: { id: favoriteRecipe?.id },
+				data: { fav: !favoriteRecipe.fav }
+			});
+			
+			return NextResponse.json({message: updated });
+		}
+	} catch (e) {
+		return NextResponse.json({ status: 500, message: 'Something Went Wrong!' });
 	}
 }
