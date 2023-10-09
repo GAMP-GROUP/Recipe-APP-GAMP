63433996bdb1106e01b6d44d17bd20133790d09c
Mario Sanseverino <marioaugusto.sanseverino@gmail.com>
2023-10-09 16:01:45 -0300

    fix: Erro com fetch de ingredientes

--- gamp-recipes/app/create/page.tsx
+++ gamp-recipes/app/create/page.tsx
@@ -2,14 +2,14 @@
 import React, { useState, useEffect } from 'react';
 import prisma from '@/prisma/client';
 
-export default async function Create() {
+export default function Create() {
 	const [ingredientsList, setIngredientsList] = useState<string[]>([]);
 
 	useEffect(() => {
 		async function fetchIngredients() {
 			try {
 				const response = await prisma.ingredients.findMany();
-				const ingredientNames = response.map((ingredient) => ingredient.ingredients_name);
+				const ingredientNames = response.map((ingredient) => ingredient.ingredients_name).sort();
 				setIngredientsList(ingredientNames);
 			} catch(error) {
 				window.alert(`ERROR: ${error}`);
@@ -21,7 +21,7 @@ export default async function Create() {
 
 	return (
 		<section className='flex-row justify-center items-center h-[600px]'>
-			<h1 className='text-center font-extrabold text-lg mt-4'>New Recipe</h1>
+			<h1 className='text-center font-extrabold text-xl mt-4'>New Recipe</h1>
 			<form id='create-recipe' className='create-form mx-auto'>
 				<fieldset className='py-2'>
 					<label htmlFor='recipe-name' className='mr-2'>Recipe Name:</label>
@@ -29,6 +29,7 @@ export default async function Create() {
 						id='recipe-name'
 						type='text'
 						placeholder='Lasagna al ragú'
+						className='border border-b-2 p-1'
 						required
 					/>
 				</fieldset>
@@ -41,7 +42,7 @@ export default async function Create() {
 				</fieldset>
 				<fieldset className='py-2'>
 					<label htmlFor='recipe-image' className='mr-2'>Image:</label>
-					<input type='text' name='recipe-image' id='recipe-image' />
+					<input type='text' name='recipe-image' id='recipe-image' className='border border-b-2 p-1' />
 				</fieldset>
 				<fieldset className='py-2'>
 					<label htmlFor='recipe-name' className='mr-2'>Instructions:</label>
@@ -55,7 +56,7 @@ export default async function Create() {
 					/>
 				</fieldset>
 				<fieldset className='py-2'>
-					<label htmlFor='ingredient-1'>Ingredient 1:</label>
+					<label htmlFor='ingredient-1' className='mr-2'>Ingredient 1:</label>
 					<select name='ingredient-1' id='ingredient-1'>
 						{ ingredientsList.map((ingredient, index) => (
 							<option
@@ -67,6 +68,11 @@ export default async function Create() {
 						))}
 					</select>
 				</fieldset>
+				<button
+					type='submit'
+					className='text-lg font-bold px-5 py-1 mr-2 bg-black text-white rounded-2xl create-button'>
+					Create
+				</button>
 			</form>
 		</section>
 	);
