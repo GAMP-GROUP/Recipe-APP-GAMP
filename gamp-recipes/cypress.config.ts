b76c4c582d990af40cad302d10cc956a489f3b90
Andrey Tetsuo <tetsuo_miyasato@hotmail.com>
2023-11-13 17:29:33 -0300

    setup: configura cypress para adicionar a opção de rodar todos os specs

--- gamp-recipes/cypress.config.ts
+++ gamp-recipes/cypress.config.ts
@@ -5,6 +5,7 @@ export default defineConfig({
 		setupNodeEvents(_on, _config) {
 			// implement node event listeners here
 		},
-		baseUrl: 'http://localhost:3000'
+		baseUrl: 'http://localhost:3000',
+		experimentalRunAllSpecs: true,
 	},
 });
