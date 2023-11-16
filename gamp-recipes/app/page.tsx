4bbc4107b714b01a4b8bc9b18afb018ba6a2d765
Andrey Tetsuo <tetsuo_miyasato@hotmail.com>
2023-11-16 17:06:10 -0300

    fix: ajusta elementos retornados para evitar duplicação de tags main

--- gamp-recipes/app/page.tsx
+++ gamp-recipes/app/page.tsx
@@ -1,32 +1,31 @@
-import prisma from "@/prisma/client"
-import RecipesFeed from "./components/RecipesFeed";
+import prisma from '@/prisma/client';
+import RecipesFeed from './components/RecipesFeed';
+import React from 'react';
 
 export default async function Home() {
-  const mealsRecipes = await prisma.recipes.findMany({
-    where: { recipe_type_id: 2 }
-  })
-  const drinksRecipes = await prisma.recipes.findMany({
-    where: { recipe_type_id: 1 }
-  })
-  const allRecipes = [...mealsRecipes, ...drinksRecipes]
+	const mealsRecipes = await prisma.recipes.findMany({
+		where: { recipe_type_id: 2 }
+	});
+	const drinksRecipes = await prisma.recipes.findMany({
+		where: { recipe_type_id: 1 }
+	});
+	const allRecipes = [...mealsRecipes, ...drinksRecipes];
   
-  function getRandomNumber() {
-    return Math.random() - 0.5;
-  }
+	function getRandomNumber() {
+		return Math.random() - 0.5;
+	}
   
-  allRecipes.sort(getRandomNumber)
+	allRecipes.sort(getRandomNumber);
 
-  return (
-    <div>
-      <main className='h-full w-full'>
-        <section className="flex-row">
-          <RecipesFeed
-            recipesQuantity={25}
-            feedType={ 'all' }
-            recipes={ allRecipes }
-          />
-        </section>
-      </main>
-    </div>
-  )
+	return (
+		<main className='h-full w-full'>
+			<section className="flex-row">
+				<RecipesFeed
+					recipesQuantity={25}
+					feedType={ 'all' }
+					recipes={ allRecipes }
+				/>
+			</section>
+		</main>
+	);
 }
