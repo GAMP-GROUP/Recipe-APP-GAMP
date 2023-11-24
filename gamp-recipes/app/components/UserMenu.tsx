d8d1e4ea87c729840bd51a3948f0e6d30ae8fc4a
Mario Sanseverino <marioaugusto.sanseverino@gmail.com>
2023-11-24 19:44:58 -0300

    fix: Ajuste nas bordas do menu lateral

--- gamp-recipes/app/components/UserMenu.tsx
+++ gamp-recipes/app/components/UserMenu.tsx
@@ -22,28 +22,25 @@ export default function UserMenu() {
 	return (
 		<main
 			id='menu-screen'
-			className={ `w-screen fixed top-16 z-[40] transition-transform duration-700 ${ menu ? 'translate-x-0' : '-translate-x-full' }` }
+			className={`w-screen fixed top-16 z-[40] transition-transform duration-700 ${menu ? 'translate-x-0' : '-translate-x-full'}`}
 		>
-			<nav
-				id='menu'
-			>
+			<nav id='menu'>
 				<ul className="flex-row w-full">
 					{menuItems.map((item, index) => (
 						<Link href={`/${item.toLowerCase()}`} key={index} onClick={removeOverflow}>
 							<li
 								onClick={() => setMenu(false)}
-								className={`flex-1 py-4 pl-4 w-full text-lg font-bold ${index === 0 ? 'font-extrabold text-black' : 'font-medium text-gray-500'
-								} flex items-center border-b-gray-500 border-1 shadow`}>
+								className={`relative flex-1 py-4 pl-4 w-full text-lg font-bold ${index === 0 ? 'font-extrabold text-black' : 'font-medium text-gray-500'
+								} flex items-center`}>
 								<picture>
 									<img
-										src={`/icons/${item.toLowerCase()}.png`}
-										alt={`${item} icon`}
-										className={`invert w-6 mr-6 ${index === 0 ? 'opacity-100' : 'opacity-20'
-										}`}
+										src={ `/icons/${item.toLowerCase()}.png` }
+										alt={ `${item} icon` }
+										className={ `invert w-6 mr-6 ${ index === 0 ? 'opacity-100' : 'opacity-20' }` }
 									/>
 								</picture>
-								{item}
-								<hr />
+								{ item }
+								<hr className="absolute w-11/12 opacity-20 border-b border-gray-500 bottom-0 left-1/2 transform -translate-x-1/2" />
 							</li>
 						</Link>
 					))}
