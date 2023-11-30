396c4ea03ed1738da5d139bb2cb9733a4e825fe0
gabriel <soutogabriel04@gmail.com>
2023-11-30 15:34:14 -0300

    feat: get author endpoint

--- gamp-recipes/app/api/recipe/recipe.service.ts
+++ gamp-recipes/app/api/recipe/recipe.service.ts
@@ -275,6 +275,7 @@ export async function getRecipeById(request: number) {
 				id: request,
 			},
 			include: {
+				Author_Recipe: true,
 				category_name: true,
 				recipe_type: true,
 				Ingredients_Recipes: {
