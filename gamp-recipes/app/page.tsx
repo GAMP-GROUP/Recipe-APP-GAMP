47aeb08569668ddf55685919a751101aef9f90dd
Mario Sanseverino <marioaugusto.sanseverino@gmail.com>
2023-12-04 17:18:08 -0300

    fix: app/page.tsx atualizada para resolução de conflitos

--- gamp-recipes/app/page.tsx
+++ gamp-recipes/app/page.tsx
@@ -1,12 +1,6 @@
-<<<<<<< HEAD
 import React from 'react';
 import prisma from '@/prisma/client';
 import RecipesFeed from './components/RecipesFeed';
-=======
-import prisma from '@/prisma/client';
-import RecipesFeed from './components/RecipesFeed';
-import React from 'react';
->>>>>>> main
 
 export default async function Home() {
 	const mealsRecipes = await prisma.recipes.findMany({
@@ -16,7 +10,6 @@ export default async function Home() {
 		where: { recipe_type_id: 1 }
 	});
 	const allRecipes = [...mealsRecipes, ...drinksRecipes];
-<<<<<<< HEAD
 
 	function getRandomNumber() {
 		return Math.random() - 0.5;
@@ -31,25 +24,4 @@ export default async function Home() {
 			recipes={allRecipes}
 		/>
 	);
-=======
-  
-	function getRandomNumber() {
-		return Math.random() - 0.5;
-	}
-  
-	allRecipes.sort(getRandomNumber);
-
-	return (
-		<main className='h-full w-full'>
-			<section className="flex-row">
-				<RecipesFeed
-					recipesQuantity={25}
-					feedType={ 'all' }
-					recipes={ allRecipes }
-				/>
-			</section>
-		</main>
-	);
-
->>>>>>> main
 }
