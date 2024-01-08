44799d0571ae80aa955869ac69d7e682df67db95
gabriel <soutogabriel04@gmail.com>
2024-01-08 18:26:36 -0300

    Refactor: UserMenu component and add detailed parameter to ProfilePage

--- gamp-recipes/app/components/UserMenu.tsx
+++ gamp-recipes/app/components/UserMenu.tsx
@@ -1,5 +1,4 @@
 'use client';
-<<<<<<< HEAD
 import Link from '@/node_modules/next/link';
 import { useBehaviorContext } from '@/contextAPI/context/behavior.context';
 import { Dispatch, SetStateAction } from 'react';
@@ -53,76 +52,5 @@ export default function UserMenu() {
 			>
 			</aside>
 		</main>
-=======
-import React, { useEffect } from 'react';
-import Link from '@/node_modules/next/link';
-import { useScrollBlock } from '../hooks/useScrollBlock';
-import { useBehaviorContext } from '@/contextAPI/context/behavior.context';
-
-export default function UserMenu() {
-	const { menu, setMenu } = useBehaviorContext();
-	const [, allowScroll] = useScrollBlock();
-	const menuItems = ['Profile', 'Meals', 'Drinks', 'Favorites', 'Create'];
-
-	function closeMenu() {
-		const menuIcon = document.querySelector('#menu-icon') as HTMLInputElement;
-
-		if (menuIcon) {
-			menuIcon.checked = false;
-		}
-
-		setMenu(false);
-		allowScroll();
-	}
-
-	useEffect(() => {
-		const handleKeyDown = (event: KeyboardEvent) => {
-			if (event.key === 'Escape') {
-				closeMenu();
-			}
-		};
-
-		document.addEventListener('keydown', handleKeyDown);
-
-		return () => {
-			document.removeEventListener('keydown', handleKeyDown);
-		};
-	}, []);
-
-	return (
-		<menu
-			id='user-menu'
-			className={`w-screen fixed top-0 left-0 right-0 transition-transform duration-300 ${ menu ? 'translate-x-0' : '-translate-x-full' }
-			2xl:hidden` }
-		>
-			<ul className='flex-row h-full w-screen'>
-				{ menuItems.map((item, index) => (
-					<Link href={`/${item.toLowerCase()}`} key={index}>
-						<li
-							onClick={() => closeMenu()}
-							onTouchStart={() => closeMenu()}
-							className={ `relative flex-1 py-4 pl-4 w-full text-lg font-bold ${ index === 0 ? 'font-extrabold text-black' : 'font-medium text-gray-500' } flex items-center` }
-						>
-							<picture>
-								<img
-									src={ `/icons/${item.toLowerCase()}.svg` }
-									alt={ `${item} icon` }
-									className={ `w-6 mr-6 ${index === 0 ? 'opacity-100' : 'opacity-20'}` }
-								/>
-							</picture>
-							{item}
-							<hr className="absolute w-11/12 opacity-20 border-b border-gray-500 bottom-0 left-1/2 transform -translate-x-1/2" />
-						</li>
-					</Link>
-				))}
-			</ul>
-			<aside
-				id='outside-menu'
-				className={`h-screen w-screen
-				xl:hidden` }
-				onClick={() => closeMenu()}
-			></aside>
-		</menu>
->>>>>>> main
 	);
 }
--- gamp-recipes/app/profile/page.tsx
+++ gamp-recipes/app/profile/page.tsx
@@ -1,31 +1,41 @@
 import prisma from '@/prisma/client';
+import { detailedParams } from '@/types';
 import React from 'react';
 
 
-export default async function ProfilePage() {
+type UserProfile = {
+	username: string
+	created_at: Date
+}
+
+export default async function ProfilePage({ params: { detailed } }: detailedParams) {
+
 	const user = await prisma.user.findUnique({
 		where: {
-			id: 1
+			id: parseInt(detailed) || 1
 		}
 	});
+	
 
 	console.log(user);
 
-	function created_since(): string {
-		const today = Date.now();
-		const created = user?.created_at as Date;
-		const diff = ((today - created) / 86400000).toFixed(0);
-		if (diff === '0') return 'Today';
-		return `Since ${diff} days ago`;
+	function created_since(user: UserProfile){
+		const timestampObj: Date = new Date(user.created_at);
+		const formattedDate: string = timestampObj.toLocaleDateString('en-US', {
+			year: 'numeric',
+			month: '2-digit',
+			day: '2-digit',
+		});
+		return `Since ${formattedDate}`;
 	}
 
-	function changeActive(type: string, active: boolean): string {
-		if (type === 'container' && active) {
-			return 'flex w-20 justify-center items-center pb-3 border-solid border-b-pink-500 border-b-8';
-		} else {
-			return 'flex w-20 justify-center items-center pb-3 border-solid border-b-white border-b-8';
-		}
-	}
+	// function changeActive(type: string, active: boolean): string {
+	// 	if (type === 'container' && active) {
+	// 		return 'flex w-20 justify-center items-center pb-3 border-solid border-b-pink-500 border-b-8';
+	// 	} else {
+	// 		return 'flex w-20 justify-center items-center pb-3 border-solid border-b-white border-b-8';
+	// 	}
+	// }
 
 
 	return (
@@ -41,7 +51,7 @@ export default async function ProfilePage() {
 				<section className='flex-row justify-evenly pt-24 pb-4 text-center'>
 					<h1 className='text-center mx-auto font-bold text-2xl'>{user?.email}</h1>
 					<p className='text-sm'>{user?.nationality}</p>
-					<p className='text-sm'>{created_since(user?.created_at)}</p>
+					<p className='text-sm'>{created_since(user as UserProfile)}</p>
 					<section className='flex justify-evenly pt-8 items-center'>
 						<div className='active-container'>
 							<img src='/icons/favorites-active.png' alt='Fav icon' className='active-profile-icon ' />
