51080a9d937d8bcc9347e0f953134311e55f6236
Mario Sanseverino <marioaugusto.sanseverino@gmail.com>
2024-01-12 16:03:59 -0300

    style: Ilustração adicionada ao slide 3

--- gamp-recipes/app/onboarding/slides/Slide3.tsx
+++ gamp-recipes/app/onboarding/slides/Slide3.tsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import FinishOnboardingButtonSection from '../components/FinishOnboardingButtonSection';
+
 export default function Slide3() {
+
 	return (
 		<section
-			className='swiper-slide'
+			className='swiper-slide text-white'
 		>
 			<h1>Unlock all content</h1>
-			<img src='/images/slide2.png' className='invert'/>
-			<div className='pb-8'>
-				<p>Sign up now for</p>
-				<p><span className='text-3xl font-bold'>free</span></p>
+			<img src='/images/slide3.png' className='invert'/>
+			<div className='pb-8 flex-col'>
+				<p>Sign up now</p>
+				<p><span className='text-[3rem] font-bold leading-tight'>it&apos;s free</span></p>
 			</div>
 			<FinishOnboardingButtonSection />
 		</section>
