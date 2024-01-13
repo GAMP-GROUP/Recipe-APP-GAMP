18a26165cee8f1267811d9a85ff5cd8ed6ef335d
Mario Sanseverino <marioaugusto.sanseverino@gmail.com>
2024-01-13 13:50:35 -0300

    refactor: Página signin ajustada e início da componentização dos containers

--- gamp-recipes/app/auth/signin/page.tsx
+++ gamp-recipes/app/auth/signin/page.tsx
@@ -1,14 +1,13 @@
 import React from 'react';
 import SignInForm from '@/app/components/SignInForm';
 
-function SingIn() {
+function SignIn() {
 	return (
-
-		<div className='z-1 flex flex-col gap-4 w-full h-full bg-slate-100 absolute top-0 left-0 right-0 bottom-0 items-center justify-center'>
+		<div className='flex flex-col w-full h-full bg-slate-100 items-center justify-center'>
 			<SignInForm />
 		</div>
 	
 	);
 }
 
-export default SingIn;
+export default SignIn;
--- gamp-recipes/app/auth/signup/page.tsx
+++ gamp-recipes/app/auth/signup/page.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import SignUpForm from '@/app/components/SignUpForm';
 
-function SingUp() {
+function SignUp() {
 	return (
 		<div className='z-3 h-full w-full bg-white'>
 			<div className='z-1 flex flex-col gap-4 w-full h-full bg-white absolute top-0 left-0 right-0 bottom-0 items-center justify-center'>
@@ -11,4 +11,4 @@ function SingUp() {
 	);
 }
 
-export default SingUp;
+export default SignUp;
