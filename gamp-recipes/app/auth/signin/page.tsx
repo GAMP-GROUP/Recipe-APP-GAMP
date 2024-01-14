8fc22af97d43d48821d43f20d8cbefd2ae85078a
Mario Sanseverino <marioaugusto.sanseverino@gmail.com>
2024-01-14 16:25:33 -0300

    refactor: /auth/signin/page.tsx componentizada

--- gamp-recipes/app/auth/signin/page.tsx
+++ gamp-recipes/app/auth/signin/page.tsx
@@ -1,10 +1,50 @@
-import React from 'react';
+'use client';
+import React, { useContext } from 'react';
+import { signIn } from 'next-auth/react';
+import { UserContext } from '@/contextAPI/context';
+import SignInSignUpHeader from '@/app/components/SignInSignUpHeader';
 import SignInForm from '@/app/components/SignInForm';
+import SignInSignUpButtonSection from '@/app/components/SignInSignButtonSection';
 
 function SignIn() {
+	const { user: { email, password } } = useContext(UserContext);
+
+	async function handleSignInBtn() {
+		// Função responsável por verificar se os campos
+		// de login estão preenchidos e faz o redirecionamento em caso de sucesso
+		if (email === '' || password === '') {
+			return window.alert('Please fill in all fields');
+		}
+
+		try {
+			const signInResponse = await signIn('credentials', {
+				redirect: false,
+				email,
+				password,
+				callbackUrl: 'http://localhost:3000/',
+			});
+
+			if (!signInResponse?.url || signInResponse.ok !== true) {
+				return window.alert('Invalid credentials');
+			}
+
+		} catch (err) {
+			return window.alert(err);
+		}
+
+	}
+
 	return (
 		<div className='flex flex-col w-full h-full bg-slate-100 items-center justify-center'>
-			<SignInForm />
+			<section className='flex flex-col items-center gap-8 bg-white shadow-xl pt-6 pb-10 px-2 rounded-xl font-lato w-11/12'>
+				<SignInSignUpHeader
+					type='signin'
+				/>
+				<SignInForm	/>
+				<SignInSignUpButtonSection
+					type='signin' handleClickBtn={ handleSignInBtn }
+				/>
+			</section>
 		</div>
 	
 	);
--- gamp-recipes/app/components/SignInForm.tsx
+++ gamp-recipes/app/components/SignInForm.tsx
@@ -1,11 +1,9 @@
 'use client';
 import React from 'react';
 import { UserContext } from '@/contextAPI/context';
-import { useSession, signIn } from 'next-auth/react';
+import { useSession } from 'next-auth/react';
 import { useRouter } from 'next/navigation';
 import { useContext, useEffect } from 'react';
-import SignInSignUpHeader from './SignInSignUpHeader';
-import SignInSignUpButtonSection from './SignInSignButtonSection';
 
 export default function SignInForm() {
 	const router = useRouter();
@@ -13,35 +11,10 @@ export default function SignInForm() {
 	const { handleInputChange, user: { email, password } } = useContext(UserContext);
 
 	function handleSubmit(event: React.FormEvent) {
-		// Função que previne a alteração da página
+		// Função que previne o recarregamento da página
 		return (
 			event.preventDefault()
 		);
-	}
-
-	async function handleSignInBtn() {
-		// Função responsável por verificar se os campos
-		// de login estão preenchidos e faz o redirecionamento em caso de sucesso
-		if (email === '' || password === '') {
-			return window.alert('Please fill in all fields');
-		}
-
-		try {
-			const signInResponse = await signIn('credentials', {
-				redirect: false,
-				email,
-				password,
-				callbackUrl: 'http://localhost:3000/',
-			});
-
-			if (!signInResponse?.url || signInResponse.ok !== true) {
-				return window.alert('Invalid credentials');
-			}
-
-		} catch (err) {
-			return window.alert(err);
-		}
-
 	}
 
 	useEffect(() => {
@@ -52,51 +25,41 @@ export default function SignInForm() {
 	}, [status]);
 
 	return (
-		<section className='flex flex-col items-center gap-8 bg-white shadow-xl pt-6 pb-10 px-2 rounded-xl font-lato w-11/12'>
-			<SignInSignUpHeader
-				type='signin'
-			/>
-
-			<form onSubmit={ handleSubmit } className='flex flex-col gap-8'>
-				<section className='bg-white text-slate-950 flex flex-col gap-8 items-center w-full'>
-					<fieldset
-						className='w-full'
-					>
-						<label className='text-gray-700 text-sm font-bold self-start'>
+		<form onSubmit={ handleSubmit } className='flex flex-col gap-8'>
+			<section className='bg-white text-slate-950 flex flex-col gap-8 items-center w-full'>
+				<fieldset
+					className='w-full'
+				>
+					<label className='text-gray-700 text-sm font-bold self-start'>
 							Email:
-						</label>
-						<input
-							type='email'
-							name='email'
-							value={email}
-							onChange={handleInputChange}
-							id='helper-text'
-							aria-describedby='helper-text-explanation'
-							className='shadow border rounded w-full py-2 px-3 text-stone-800 leading-tight focus:outline-none'
-							placeholder='example@email.com'
-						/>
-					</fieldset>
-					<fieldset
-						className='w-full'
-					>
-						<label className='text-gray-700 text-sm font-bold self-start'>
+					</label>
+					<input
+						type='email'
+						name='email'
+						value={email}
+						onChange={handleInputChange}
+						id='helper-text'
+						aria-describedby='helper-text-explanation'
+						className='shadow border rounded w-full py-2 px-3 text-stone-800 leading-tight focus:outline-none'
+						placeholder='example@email.com'
+					/>
+				</fieldset>
+				<fieldset
+					className='w-full'
+				>
+					<label className='text-gray-700 text-sm font-bold self-start'>
 							Password:
-						</label>
-						<input
-							type='password'
-							name='password'
-							value={password}
-							onChange={handleInputChange}
-							placeholder='******'
-							className='shadow border rounded w-full py-2 px-3 text-stone-800 leading-tight focus:outline-none'
-						/>
-					</fieldset>
-				</section>
-			</form>
-
-			<SignInSignUpButtonSection
-				type='signin' handleClickBtn={ handleSignInBtn }
-			/>
-		</section>
+					</label>
+					<input
+						type='password'
+						name='password'
+						value={password}
+						onChange={handleInputChange}
+						placeholder='******'
+						className='shadow border rounded w-full py-2 px-3 text-stone-800 leading-tight focus:outline-none'
+					/>
+				</fieldset>
+			</section>
+		</form>
 	);
 }
