6815479ce1088985d999f2d6dc9a3684f2b74cc3
Andrey Tetsuo <tetsuo_miyasato@hotmail.com>
2024-04-15 19:13:57 -0300

    feat(recipes): refatora estilo, consulta ao db e inclui ícone

--- gamp-recipes/app/components/RecipesCard.tsx
+++ gamp-recipes/app/components/RecipesCard.tsx
@@ -1,6 +1,5 @@
 import Link from 'next/link';
 import React from 'react';
-import FavButton from './FavoriteButton';
 
 type recipeProps = {
 	id?: number | null,
@@ -14,30 +13,56 @@ type recipeProps = {
 }
 
 
-export default function RecipesCard({ id, image, title, type }: recipeProps): JSX.Element {
+export default function RecipesCard({ id, image, title, category, alcoholic }: recipeProps): JSX.Element {
+	function capitalize(word: string): string {
+		const capitalized = word.split('');
+		capitalized[0] = capitalized[0].toUpperCase();
+
+		return capitalized.join('');
+	}
+	
 	return (
-		<div className={ `text-center mb-4 relative flex flex-col gap-2
+		<div className={ `text-center mb-4 relative flex flex-col gap-2 
 		xl:w-64 xl:mx-auto` }>
-			<Link href={`/${id}`} replace>
-				<picture>
-					<img
-						alt={title}
-						src={image}
-						className='object-cover rounded-3xl w-96 h-48 shadow-lg'
-					/>
-				</picture>
-			</Link>
-			<section className='px-4 flex items-left text-left justify-between'>
-				<div className='flex flex-col gap-2'>
-					<h2 className="text-[1.75rem] font-black">{title}</h2>
-					<p className={ `text-[0.8rem] w-20 text-white text-center rounded-xl uppercase font-[600] tracking-[0.075rem] ${ type === 2 ? 'bg-red' : 'bg-blue-600' }` }>
-						{ type === 2 ? 'meal' : 'drink' }
-					</p>
+			<Link href={`/${id}`}>
+				<div
+					style={{
+						backgroundImage: `url(${image})`,
+						backgroundRepeat: 'no-repeat',
+						backgroundPosition: 'top center',
+						backgroundSize: '100%',
+						width: '336px',
+						height: '256px',
+					}}
+					className='rounded-3xl shadow-lg flex justify-center'
+				>	<section
+						className='grid grid-cols-2 self-center h-[90%]  w-[90%]'
+					>
+						<p
+							className='text-heading3 self-end justify-self-start text-white shadow-lg'
+						>
+							{title}
+						</p>
+						
+						<p
+							className={`text-strong text-white self-start justify-self-end
+							bg-${ alcoholic ? alcoholic?.toLowerCase().replace(' ', '') : category?.toLowerCase()} 
+							py-2 pr-2 rounded-xl flex place-content-evenly`}
+						>
+							<img
+								className='h-[12px] self-center px-2'
+								src='/icons/category_icon.png'
+							/>
+							{ alcoholic || capitalize(category as string) }
+						</p>
+					</section>
+					<span
+						className='bg-pork bg-alcoholic bg-nonalcoholic bg-beef bg-vegetarian bg-chicken bg-pasta'
+					>
+
+					</span>
 				</div>
-				<FavButton
-					id={ id!.toString() }
-				/>
-			</section>
+			</Link>
 		</div>
 	);
 }
